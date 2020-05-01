879d4aaa75a6d66852002962ae96c61413ff5b71
yasu <yasuhiro.senoo@d-ss.co.jp>
2020-05-02 01:52:15 +0900

    aa

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -7,7 +7,7 @@ terraform {
   required_version = ">= 0.12"
 
   backend "s3" {
-    bucket  = "tfstate-bucket-ys-ys"
+    bucket  = "tfstate-bucket-ys"
     region  = "ap-northeast-1"
     key     = "ec2/terraform.tfstate"
     encrypt = true
