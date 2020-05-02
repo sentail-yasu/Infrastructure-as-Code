2b1ca0fc2a43d3a3106c9f629ea91e4df28dde74
yasu <yasuhiro.senoo@d-ss.co.jp>
2020-05-02 22:24:47 +0900

    publicサブネット紐づけ

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -53,12 +53,23 @@ resource "aws_internet_gateway" "igw" {
 }
 
 # Route Table
-resource "aws_route_table" "rt" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
 }
 
 resource "aws_route" "route" {
   gateway_id             = aws_internet_gateway.igw.id
-  route_table_id         = aws_route_table.rt.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
+}
+
+# パブリックルートテーブルと関連付け
+resource "aws_route_table_association" "public_a" {
+    subnet_id      = aws_subnet.public-a.id
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+    subnet_id      = aws_subnet.public_c.id
+    route_table_id = aws_route_table.public.id
 }
