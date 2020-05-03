5f2416defc909979d281c6b291c8a5bf6c529017
yasu <yasuhiro.senoo@d-ss.co.jp>
2020-05-03 22:42:37 +0900

    パブリックアクセス拒否

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -32,3 +32,12 @@ resource "aws_s3_bucket" "private" {
     }
   }
 }
+
+# ブロックパブリックアクセス
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
